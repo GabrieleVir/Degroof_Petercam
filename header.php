e4fbb46bfa871ca3c0a789fffe07418f412bc52f
LudovicPatho <ludovic.patho@gmail.com>
2017-09-15 14:54:40 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,6 @@
+	<!-- Logo -->
+							<div id="logo">
+								<a href="index.html"><img src="images/degroof.png" class="logoDegroof"></a>
+								<p class="subLogo"><strong class="white">Since 2008</strong>, we support and guide long-term & innovative projects with large-scale implementation potential.
+We also seek to promote engagement within Bank Degroof Petercam. </p>
+							</div>
